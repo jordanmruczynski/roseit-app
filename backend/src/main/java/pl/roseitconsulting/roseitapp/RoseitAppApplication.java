8430bc3e1648f959be0808672adace381d3e730e
Jordan Mruczynski <joromruc@gmail.com>
2023-08-10 20:07:09 +0200

    - Change postgres image version to 15:3, not latest - Create github action workflow backend-ci.yml - Remove unused import from RoseitAppApplication.java - Add Foo timestamp file to frontend directory

--- backend/src/main/java/pl/roseitconsulting/roseitapp/RoseitAppApplication.java
+++ backend/src/main/java/pl/roseitconsulting/roseitapp/RoseitAppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.*;
 import pl.roseitconsulting.roseitapp.customer.Customer;
 import pl.roseitconsulting.roseitapp.customer.CustomerRepository;
 
